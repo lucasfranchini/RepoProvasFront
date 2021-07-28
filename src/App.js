5fbb6e8fd88a442a240b730a0d4626f62ee25f97
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-28 13:26:49 -0300

    link to create and search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
 import GlobalStyles from "./styles/GlobalStyle";
 
 
@@ -8,7 +9,13 @@ function App() {
     <GlobalStyles/>
       <Switch>
         <Route path='/' exact>
-
+          <Home/>
+        </Route>
+        <Route path='/create' exact>
+          <Home/>
+        </Route>
+        <Route path='/search' exact>
+          <Home/>
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function Home(){
+    return(
+        <Body>
+            <Link to='/'>RepoProvas</Link>
+            <Buttons>
+                <Link to='/create'>Enviar Provas</Link>
+                <Link to='/search'>Procurar Provas</Link>
+            </Buttons>
+        </Body>
+    );
+}
+
+const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    a{
+        font-size: 100px;
+        color: #333;
+    }
+`
+const Buttons =styled.div`
+    width: 800px;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 20px;
+    a{
+        width: 250px;
+        font-size: 25px;
+        background-image: linear-gradient(to bottom right,#333,#4f4f4f);
+        color: #fff;
+        border-radius: 5px;
+        border: none;
+        text-align: center;
+        padding: 5px;
+        cursor: pointer;
+    }
+`
