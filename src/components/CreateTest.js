42985d146d4060f82e96b68d3f1ff9d06ba081d4
lucasfranchini <lucasfranchini.a@gmail.com>
2021-07-30 11:36:46 -0300

    select categories

--- src/components/CreateTest.js
+++ src/components/CreateTest.js
@@ -1,22 +1,42 @@
-import { TextField } from "@material-ui/core";
-import { useState } from "react";
+import { InputLabel, MenuItem, Select, TextField,FormControl } from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export default function CreateTest(){
     const [test,setTest] = useState({
         name: '',
-        Link: '',
-        categoryId: 0,
-        subjectId:0,
-        ProfessorId:0
+        link: '',
+        category: '',
+        subject:'',
+        Professor:''
     })
+    const [categories,setCategories] = useState([]);
+    const [subjects,setSubjects] = useState([]);
+    useEffect(()=>{
+        const res = axios.get(`${process.env.REACT_APP_API_BASE_URL}/category`)
+        res.then(res=>{
+            setCategories(res.data)
+        })
+        res.catch(res=>{
+            alert("houve um erro ao carregar as informacoes")
+        })
+    },[])
     return (
         <Body>
             <Link to='/'>RepoProvas</Link>
-            <form>
-                <Input required label='Nome' value={test.name} onChange={e=>setTest({...test,name:e.target.value})} variant="outlined"margin='normal'/>
-                <Input required label='Link' value={test.link} onChange={e=>setTest({...test,link:e.target.value})} variant="outlined"margin='normal'/>
+            <form >
+                <Input required label='Nome' value={test.name} onChange={e=>setTest({...test,name:e.target.value})} variant="outlined" margin='normal'/>
+                <Input required label='Link' value={test.link} onChange={e=>setTest({...test,link:e.target.value})} variant="outlined" margin='normal'/>
+                <SelectDiv variant="outlined" margin='normal' required>
+                    <InputLabel >Category</InputLabel>
+                    <SelectInput label='category' value={test.category} onChange={e=>setTest({...test,category:e.target.value})} >
+                        <MenuItem  value={null}>None</MenuItem>
+                        {categories?.map(c=><MenuItem key={c.id} value={c.name}>{c.name}</MenuItem>)}
+                    </SelectInput>
+                </SelectDiv>
+                
             </form>
         </Body>
     );
@@ -26,18 +46,31 @@ const Body = styled.div`
     margin-top: 100px;
     display: flex;
     flex-direction: column;
-    align-items: center;
+    align-items: center; 
     a{
         font-size: 100px;
         color: #333;
         margin-bottom: 100px;
     }
     form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         width: 100%;
+        
     }
+    
 `
 const Input = styled(TextField)`
-    margin-top: 10px;
     width: 90%;
     background-color: #fff;
+`
+const SelectInput = styled(Select)`
+    width: 100%;
+    background-color: #fff;
+    height: 56px;
+`
+const SelectDiv = styled(FormControl)`
+    width: 90%;
 `
